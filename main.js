e5dde27c0f3b369fcee16702b749bc69e80345c3
broleomo <brooklynnmoor@gmail.com>
2017-08-16 20:16:12 -0400

    first try

---
+++ main.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const list = {
+[
+    {todo: 'Get groceries',
+     yetTodo: true
+   },
+   {todo: "Go to the gym",
+     yetTodo: false,
+   },
+    {todo: "Go to the gym",
+    yetTodo: false},
+    {todo: 'Learn Mustache',
+    yetTodo: true}
+]
+};
+const data = {
+  todos: list
+};
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'milk')
+
+app.get('/milk/', function (req, res) {
+  res.render('milk', data);
+});
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+//
+app.post("/",function(req,res){
+  list.push(todo:req.body.todo, yetTodo:true});
+  res.redirect('/');
+  ));
+
+  app.post('complete',function(req,res){
+    console.log(req.body);
+    let completed = req.body.complete;
+    function findTodo(item){
+      return item.todo === completed;}
+      console.log(list.find(findTodo));
+      list.find(findTodo).yetTodo = false;
+      res.redirect('/');
+    }
+  })
+
+app.listen(3000,function(){
+  console.log("This is running correctly!");
+});
